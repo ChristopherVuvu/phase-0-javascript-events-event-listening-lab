bddd3bc4687d8d10debda839e4d869986a13e00c
ChristopherVuvu <vuvumumo93@gmail.com>
2023-04-04 09:19:27 +0300

    added

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const button = document.querySelector("#button");
+    button.addEventListener('click', function(){
+        console.log("Click Me!")
+    });
 }
